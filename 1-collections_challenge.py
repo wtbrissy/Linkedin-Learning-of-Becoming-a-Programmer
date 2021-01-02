a19d9e5f2d6784b4c2509f564c41d48d66e4ecd4
Wtbrissy <59547937+wtbrissy@users.noreply.github.com>
2021-01-02 10:40:21 +1000

    complete collection challenge

---
+++ 1-collections_challenge.py
@@ -0,0 +1,37 @@
+# Nearest stars to Earth
+# star1 = 'Sol'
+# star2 = 'Alpha Centauri'
+# star3 = 'Barnard'
+# star4 = 'Wolf 359'
+
+# Highest peak on each tectonic plate
+# African = 'Kilimanjaro'
+# Antarctic = 'Vinson'
+# Australian = 'Puncak Jaya'
+# Eurasian = 'Everest'
+# North_American = 'Denali'
+# Pacific = 'Mauna Kea'
+# South_American = 'Aconcagua'
+
+# Replace variables with list "stars"
+starts = [
+    'Sol',
+    'Alpha Centauri', 
+    'Barnard',
+    'Wolf 359',
+]
+# Create statement that prints fourth nearest star
+print(starts[3])
+
+# Create dictionary "peaks" that stores all values
+peaks = {
+    'African': 'Kilimanjaro',
+    'Antarctic': 'Vinson',
+    'Australian': 'Puncak Jaya',
+    'Eurasian': 'Everest',
+    'North_American': 'Denali',
+    'Pacific': 'Mauna Kea',
+    'South_American': 'Aconcagua',
+}
+# Create statement that prints name of highest peak on Pacific plate
+print(peaks['Pacific'])
