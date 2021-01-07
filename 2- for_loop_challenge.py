4cf3bf456db6e256f6542d1fdfdf83b822c792cb
Wtbrissy <59547937+wtbrissy@users.noreply.github.com>
2021-01-08 09:28:18 +1000

    Completed for loop

---
+++ 2- for_loop_challenge.py
@@ -0,0 +1,16 @@
+fruits = [
+    'apples',
+    'bananas',
+    'dragon fruit',
+    'mangos',
+    'nectarines',
+    'pears',
+]
+
+#  Print the name of all the fruits in the Termainal 
+# print each name on a separate line
+# Above the list, print "Our fruit selction:"
+
+print('Our fruit selction:')
+for fruit in fruits:
+    print(fruit)
